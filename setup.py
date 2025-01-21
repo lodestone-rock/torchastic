877af3d1c555b3d6911a36c6560235c26c75b06a
kavorite <kavorite@gmail.com>
2025-01-21 10:59:42 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchastic",
-    version="0.1.1",
+    version="0.2.0",
     author="Lodestone",
     author_email="lodestone.rock@gmail.com",
     description="Stochastic bfloat16 based optimizer library.",
